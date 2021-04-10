6770fe6c9921e3923782fd32848e9d0c9da6aa55
allthecolors <80242818+0xA1176ec01045@users.noreply.github.com>
2021-04-10 09:33:46 -0700

    Add files via upload
    Detect addresses from interaction records with the protocol that are contracts rather than external owned accounts. This distinction is necessary to ensure that distributions are awarded to the user initiating the contract call rather than to the contract itself.

---
+++ DetectContracts.py
@@ -0,0 +1,29 @@
+import web3
+import pandas as pd
+
+w3 = web3.Web3(web3.Web3.HTTPProvider('https://mainnet.infura.io/v3/YOUR-PROJECT-ID'))
+
+def isContract(address):
+    code = w3.eth.getCode(address).hex()
+    if code == '0x':
+        return False
+    else:
+        return True
+
+#earlyUserFile = 'Compound.EarlyUsers.csv'
+earlyUserFile = 'test2.csv'
+txData = pd.read_csv(earlyUserFile)
+
+# Determine whether address type is either:
+# --> User address (external owner account or EOA), or
+# --> Contract address
+addressType = []
+counter = 0
+for address in txData['address']:
+    counter += 1
+    if isContract(address):
+        addressType.append('contract')
+    else:
+        addressType.append('EOA')
+txData['addressType'] = addressType
+txData.to_csv('Compound.EarlyUsers.AccountType.csv',index=False)
